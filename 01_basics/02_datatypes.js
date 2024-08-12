6eaa3e17b7e792b35ae8c67e9f22931887c01ad2
princekumar7714 <pr4367274@gmail.com>
2024-08-12 17:27:28 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";  //treat all js code as never version
+ 
+//alert(3+3) //we are using nodejs, not browser
+
+
+console.log(3+
+    3) //code readability should be high 
+
+    console.log("prince")
+
+
+    let name = "prince"
+    let age = 18
+    let isloggedIn = true
+let state;
+
+
+    //number => 2 to power 53
+    //bigint
+    //string => ""
+    //boolean => ture/false
+    //null => standalone value
+    //undefined
+    //symbol=> unique
+    //object
+
+    console.log(typeof " prince");
+    
