0cd78baa09470f41bf89eaec075aacd308a26148
princekumar7714 <pr4367274@gmail.com>
2024-08-13 18:00:19 +0000

    conversion of data type

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,26 @@
+let score = "prince"
+
+//console.log(typeof score);
+//console.log(typeof (score))
+
+//let valueInNumber = Number(score)
+ //console.log(typeof valueInNumber);
+ //console.log(valueInNumber);
+
+ // "33"=33
+ //"33abc" =NaN
+ //true=1, or false=0
+ 
+
+ //let  isLoggedin = "prince"
+ //let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+ 
+ //1=> true or 0=> false
+ //""=> false
+ //"prince"=>true
+
+ let somenumber = 33
+  let stringNumber = string(someNumber)
+  console.log(stringNumber);
+  
