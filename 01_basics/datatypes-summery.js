e4b0dc6d0597083f112d054388d6264ca7eacf60
princekumar7714 <pr4367274@gmail.com>
2024-08-14 06:32:01 +0000

    data type summery

---
+++ 01_basics/datatypes-summery.js
@@ -0,0 +1,35 @@
+//n   primitive data types
+
+//string , number , boolean , bigint, undefined, null, symbol
+
+// const score = 100
+// const scorevalue = 100.3
+
+// const isLoggedIn =false
+// const outsidetemp = null
+// let useremail;
+ 
+// const id = symbol ('123')
+// const anotherID = symbol ('123')
+// console.log(id === anotherID);
+
+//const bignumber = 1121314444242442n
+
+//non primitive 
+
+// array , object function
+ 
+const heroes = [ "krish" , "batman" , "cpaain"];
+
+let myobj ={
+    name: "prince",
+    age :33
+};
+
+const myfunction = function(){
+    console.log("hello world");
+    
+}
+
+
+console.log(typeof Bignumber);
