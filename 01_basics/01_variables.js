b5934cfd29b2458f2240e3a22fd0f7c874437647
princekumar7714 <pr4367274@gmail.com>
2024-08-14 06:33:49 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 12121
+let accountEmail= "princerajput@gamil.com"
+var accountpass = "2536"
+accountcity = "jaipur"
+
+
+let accountState;
+
+
+
+// accountId = 2//not allowed
+
+accountEmail = "hikdefo@gmail.com"
+accountpass= "56722"
+accountcity = "up"
+
+
+/*
+ptrfer not to use var 
+because of issue in block scope and functional scope 
+
+*/
+console.table([accountId,accountEmail,accountpass,accountcity]);
+
