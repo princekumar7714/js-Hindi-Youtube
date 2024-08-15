fcad50e827237f26983bd6ebeb91a4a1932728a0
princekumar7714 <pr4367274@gmail.com>
2024-08-15 06:28:07 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,35 @@
+ //const name = "prince"
+ //const repocount =50
+
+ //console.log(name + repocount+"value");
+    
+ //console.log(` Hello my name is ${name} and my repo count is ${repocount}`);
+
+ //const gameName = new string(`princehc`)
+ //console.log(gameName[0]);
+ //console.log(gameName.__proto__);
+ //console.log(gameName.length);
+ //console.log(gameName.toUpperCase());
+ //console.log(gameName.charAt(`5`));
+ //console.log(gameName.indexOf(`i`));
+ 
+//  const NewString = gameName.substring{0, 4}
+//   console.log(NewString);
+ 
+//   const anotherString = gameName.slice{ 1, 5}
+//  console.log(anotherString);
+  
+ const newStringOne = "     prince    "
+ console.log(newStringOne);
+ console.log(newStringOne.trim());
+ 
+ const url = "htpps.//ptronce.comn//hc20%raj"
+ console.log(url.replace(`20%`,`-`));
+ console.log(url.includes(`ptronce`)
+ );
+ 
+ 
+ 
+ 
+ 
+ 
